40a6f59dbbad0bf310d5ea5e1b67dbcaef04e231
mavix21 <marcevizcarra2109@hotmail.com>
2026-01-08 18:32:47 -0500

    feat: configure astro to use react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
 
 import { defineConfig, fontProviders } from "astro/config";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
 
   vite: {
     plugins: [tailwindcss()],
