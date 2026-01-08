a0428b3da874b7ae6ecee53e627df0c7fbf27d81
mavix21 <marcevizcarra2109@hotmail.com>
2026-01-08 17:49:12 -0500

    feat: add eslint plugin react and config

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,11 +4,29 @@ import { includeIgnoreFile } from "@eslint/compat";
 import eslint from "@eslint/js";
 import eslintPluginAstro from "eslint-plugin-astro";
 import importPlugin from "eslint-plugin-import";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+const reactConfig = defineConfig(
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    ...reactPlugin.configs.flat.recommended,
+    ...reactPlugin.configs.flat["jsx-runtime"],
+    languageOptions: {
+      ...reactPlugin.configs.flat.recommended?.languageOptions,
+      ...reactPlugin.configs.flat["jsx-runtime"]?.languageOptions,
+      globals: {
+        React: "writable",
+      },
+    },
+  },
+  reactHooks.configs.flat["recommended-latest"],
+);
 
 export default defineConfig(
   includeIgnoreFile(gitignorePath),
@@ -34,5 +52,6 @@ export default defineConfig(
       "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
     },
   },
+  reactConfig,
   ...eslintPluginAstro.configs.recommended,
 );
