2e02b82f7705df71194a01eecf3ace0682cdc62e
mavix21 <marcevizcarra2109@hotmail.com>
2026-01-08 20:23:49 -0500

    feat: add astro-icon integration to enhance icon support

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,12 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), icon()],
 
   vite: {
     plugins: [tailwindcss()],
