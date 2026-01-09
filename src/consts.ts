b405f27bbf5bc80ca59c37fe9b600f05da71be00
mavix21 <marcevizcarra2109@hotmail.com>
2026-01-08 19:39:28 -0500

    feat: add Site type definition and update consts with site data

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,17 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+import type { Site } from "./types";
+
 export const SITE_TITLE = "Astro Blog";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const SITE: Site = {
+  title: "Marcelo Vizcarra",
+  description: "Personal blog of Marcelo Vizcarra",
+  href: "https://marcelovizcarra.com",
+  author: "Marcelo Vizcarra",
+  locale: "en-US",
+  featuredPostCount: 3,
+  postsPerPage: 6,
+};
